f20b59aed5e00935c5f4bf91cf7a2c6f8dd4ec41
Henry Doce <henrydoce@protonmail.com>
2019-03-10 18:49:19 -0400

    this module finished

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,36 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-var PropTypes = require('prop-types');
-
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img 
-          src={this.props.img}
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username:PropTypes.string.isRequired
-}
+var App = require('./components/App');
 
 ReactDOM.render(
-  <Badge
-    name='TylerMcGinnis'
-    username='tylermcginnis'
-    img='https://avatars0.githubusercontent.com/u/2933430?v=38s=460'
-  />,
-  document.getElementById('root')
+  <App/>,
+  document.getElementById('app')
 
 )
